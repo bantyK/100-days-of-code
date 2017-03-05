a524b21f21e69fa920863ce2301c45bc6ead89b4
Banty Kumar <bantyp92@gmail.com>
2017-03-05 12:15:33 +0530

    Transaction class tests added

--- Streams/src/TransactionManager.java
+++ Streams/src/TransactionManager.java
@@ -1,20 +1,88 @@
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class TransactionManager {
     private final List<Transaction> transactions;
-    private final List<Trader> traders;
-    public TransactionManager(List<Transaction> transactions,List<Trader> traders) {
+
+    public TransactionManager(List<Transaction> transactions) {
         this.transactions = transactions;
-        this.traders = traders;
     }
 
     public List<Transaction> get2011TransactionsSortedByValue() {
         return transactions.stream()
                 .filter(transaction -> transaction.getYear() == 2011)
-                .sorted((t1,t2) -> Integer.compare(t1.getValue(),t2.getValue()))
+                .sorted((t1, t2) -> Integer.compare(t1.getValue(),t2.getValue()))
                 .collect(Collectors.toList());
     }
 
+
+    public List<String> getUniqueCitiesOfTraders() {
+        return transactions.stream()
+                .map(transaction -> transaction.getTrader().getCity())
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    public List<Trader> getAllTradersFromCambridgeSortedByName() {
+        return transactions.stream()
+                .map(Transaction::getTrader)
+                .filter(trader -> trader.getCity().equals("Cambridge"))
+                .distinct()
+                .sorted(Comparator.comparing(Trader::getName))
+                .collect(Collectors.toList());
+    }
+
+    public String getAllDistinctTradersNameAsAStringSortedAlphabeticallly() {
+        return transactions.stream()
+                .map(transaction -> transaction.getTrader().getName())
+                .distinct()
+                .sorted()
+                .reduce("", (n1, n2) -> n1 + n2)
+                .toString();
+
+    }
+
+    public boolean isTraderFromCity(String cityName) {
+        return transactions.stream()
+                .anyMatch(transaction -> transaction.getTrader().getCity().equals(cityName));
+    }
+
+    public void printTransactionsFromTraderOfCity(String cityName) {
+        transactions.stream()
+                .filter(transaction -> transaction.getTrader().getCity().equals(cityName))
+                .forEach(System.out::println);
+
+    }
+
+    public String printTransactionsValuesFromTraderOfCity(String cityName) {
+        return transactions.stream()
+                .filter(transaction -> transaction.getTrader().getCity().equals(cityName))
+                .map(transaction -> transaction.getValue())
+                .map(String::valueOf)
+                .reduce("", (v1, v2) -> v1 + v2);
+    }
+
+    public int getSumOfValuesOfTransactionsOfTradersFromACity(String cityName) {
+        return transactions.stream()
+                .filter(transaction -> transaction.getTrader().getCity().equals(cityName))
+                .map(Transaction::getValue)
+                .reduce(0, (total, value) -> total + value);
+    }
+
+    public int getHighestTransactionValue() {
+        return transactions.stream()
+                .map(Transaction::getValue)
+                .reduce(Integer::max)
+                .get();
+
+        //method 2
+
+//        return transactions.stream()
+//                .map(Transaction::getValue)
+//                .sorted((v1,v2) -> Integer.compare(v2,v1))
+//                .findFirst()
+//                .get();
+    }
 
 }
--- Streams/tests/TransactionManagerTest.java
+++ Streams/tests/TransactionManagerTest.java
@@ -1,7 +1,7 @@
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -11,21 +11,30 @@ import static org.junit.Assert.*;
  */
 public class TransactionManagerTest {
     private TransactionManager transactionManager;
+    private List<Transaction> transactions = new ArrayList<>();
+
     Trader raoul = new Trader("Raoul", "Cambridge");
     Trader mario = new Trader("Mario", "Milan");
     Trader alan = new Trader("Alan", "Cambridge");
     Trader brian = new Trader("Brian", "Cambridge");
-    List<Transaction> transactions = Arrays.asList(
-            new Transaction(brian, 2011, 300),
-            new Transaction(raoul, 2012, 1000),
-            new Transaction(raoul, 2011, 400),
-            new Transaction(mario, 2012, 710),
-            new Transaction(mario, 2012, 700),
-            new Transaction(alan, 2012, 950)
-    );
+
+    Transaction t1 = new Transaction(brian, 2011, 300);
+    Transaction t2 = new Transaction(raoul, 2012, 1000);
+    Transaction t3 = new Transaction(raoul, 2011, 400);
+    Transaction t4 = new Transaction(mario, 2012, 710);
+    Transaction t5 = new Transaction(mario, 2012, 700);
+    Transaction t6 = new Transaction(alan, 2012, 950);
+
 
     @Before
     public void setUp() throws Exception {
+
+        transactions.add(t1);
+        transactions.add(t2);
+        transactions.add(t3);
+        transactions.add(t4);
+        transactions.add(t5);
+        transactions.add(t6);
         transactionManager = new TransactionManager(transactions);
     }
 
@@ -33,8 +42,72 @@ public class TransactionManagerTest {
     public void testGetTransactionsin2011SortedByValue() throws Exception {
         List<Transaction> result = transactionManager.get2011TransactionsSortedByValue();
 
-        assertEquals(2,result.size());
-        assertEquals(300,result.get(0).getValue());
-        assertEquals(400,result.get(1).getValue());
+        assertEquals(2, result.size());
+        assertEquals(300, result.get(0).getValue());
+        assertEquals(400, result.get(1).getValue());
+    }
+
+    @Test
+    public void testGetUniqueCitiesOfTraders() throws Exception {
+        List<String> resultList = transactionManager.getUniqueCitiesOfTraders();
+        assertEquals(2, resultList.size());
+        assertEquals("Cambridge", resultList.get(0));
+        assertEquals("Milan", resultList.get(1));
+    }
+
+    @Test
+    public void testGetAllTradersFromCambridgeSortedByName() throws Exception {
+        List<Trader> resultList = transactionManager.getAllTradersFromCambridgeSortedByName();
+
+        assertEquals(3, resultList.size());
+        assertEquals("Alan", resultList.get(0).getName());
+        assertEquals("Brian", resultList.get(1).getName());
+        assertEquals("Raoul", resultList.get(2).getName());
+    }
+
+    @Test
+    public void testGetAllDistinctTradersNameAsAStringSortedAlphabeticallly() throws Exception {
+        String tradersName = transactionManager.getAllDistinctTradersNameAsAStringSortedAlphabeticallly();
+        assertEquals("AlanBrianMarioRaoul", tradersName);
+
+    }
+
+    @Test
+    public void testCheckTradersFromCity() throws Exception {
+        boolean isTraderFromMilan = transactionManager.isTraderFromCity("Milan");
+        assertTrue(isTraderFromMilan);
+        boolean isTraderFromPune = transactionManager.isTraderFromCity("Pune");
+        assertFalse(isTraderFromPune);
+    }
+
+    @Test
+    public void testPrintTransactionFromTradersFromACity() throws Exception {
+        //this is not a test..we will just print the transactions from a trader based on city name
+        transactionManager.printTransactionsFromTraderOfCity("Cambridge");
+
+    }
+
+    @Test
+    public void testPrintTransactionValuesFromTradersFromACity() throws Exception {
+        String transactionValuesConcatenated =
+                transactionManager.printTransactionsValuesFromTraderOfCity("Cambridge");
+
+        assertEquals("3001000400950", transactionValuesConcatenated);
+    }
+
+    @Test
+    public void testGetSumOfValuesOfTransactionsOfTradersFromACity() throws Exception {
+        int totalValueFromCambridge =
+                transactionManager.getSumOfValuesOfTransactionsOfTradersFromACity("Cambridge");
+        assertEquals(300 + 1000 + 400 + 950, totalValueFromCambridge);
+
+    }
+
+    @Test
+    public void testGetHighestTransactionValue() throws Exception {
+        int highestTransactinValue = transactionManager.getHighestTransactionValue();
+
+        assertEquals(1000, highestTransactinValue);
+
     }
 }
